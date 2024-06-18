d3363df00a413dfa13a0bb01a5fa3f2a0404593a
YasseenR <yasseenr@gmail.com>
2024-06-18 15:48:26 -0400

    Created a button with a label that calls a function

--- GuessTheFlag/ContentView.swift
+++ GuessTheFlag/ContentView.swift
@@ -10,18 +10,20 @@ import SwiftUI
 struct ContentView: View {
     
     var body: some View {
-        VStack(spacing: 0) {
-            HStack {
-                AngularGradient(gradient: Gradient(colors: [.red, .yellow, .green, .blue, .purple, .red]), center: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)    }
-            
-            HStack {
-                RadialGradient(colors: [.red, .green, .blue], center: .center, startRadius: 20, endRadius: 200)
-            }
-            HStack {
-                LinearGradient(colors: [.red, .green, .blue], startPoint: .top, endPoint: .bottom)
+        VStack {
+            Button {
+                print("Edit Button was tapped!")
+            } label: {
+                Image(systemName: "pencil")
             }
         }
+        
     }
+    
+    func executeDelete() {
+        print("Now deleting")
+    }
+    
 }
 
 #Preview {
